7510d4531023c70b7144b2e8132362bc56cd7d3d
Maxime Eloir <maxime.eloir@gmail.com>
2022-04-24 17:33:13 +0200

    OnePush

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -64,16 +64,23 @@ export default {
   },
   mounted() {
     // Inside page components
-this.$OneSignal.push(() => {
-    this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
-    if (isEnabled) {
-      console.log('Push notifications are enabled!')
-    } else {
-      console.log('Push notifications are not enabled yet.')
-    }
-  })
-})
+    this.$OneSignal.push(() => {
+      this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
+        if (isEnabled) {
+          console.log("Push notifications are enabled!");
+        } else {
+          console.log("Push notifications are not enabled yet.");
+        }
+      });
+    });
 
+    // Using window and array form
+    window.$OneSignal.push([
+      "addListenerForNotificationOpened",
+      (data) => {
+        console.log("Received NotificationOpened:", data);
+      },
+    ]);
     var hamburger = document.getElementById("hamburger-menu");
     var lateral = document.getElementById("lateral");
     var menu_link = document.getElementById("menu-link");
